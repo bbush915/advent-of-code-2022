aa7ff850ab5d1c1f75e69ccaffe6294f6122018d
bbush915 <bushb@umich.edu>
2022-11-30 23:08:25 -0600

    feat: Day 1 TS!

---
+++ src/day.1.ts
@@ -0,0 +1,32 @@
+import fs from "fs";
+
+function parseInput() {
+  return fs
+    .readFileSync("src/day.1.input.txt")
+    .toString()
+    .split("\n\n")
+    .map((x) =>
+      x
+        .split("\n")
+        .filter((x) => x)
+        .map(Number)
+    );
+}
+
+export function part1() {
+  const elfCalories = parseInput();
+
+  return elfCalories
+    .map((x) => x.reduce((sum, calories) => (sum += calories), 0))
+    .sort((x, y) => y - x)[0];
+}
+
+export function part2() {
+  const elfCalories = parseInput();
+
+  return elfCalories
+    .map((x) => x.reduce((sum, calories) => (sum += calories), 0))
+    .sort((x, y) => y - x)
+    .slice(0, 3)
+    .reduce((sum, totalCalories) => (sum += totalCalories), 0);
+}
