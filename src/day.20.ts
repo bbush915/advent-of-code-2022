6c52a6bf2c178efb98614a13efa7902d2c71a097
bbush915 <bushb@umich.edu>
2022-12-19 23:55:14 -0600

    feat: Day 20 TS

--- src/day.20.ts
+++ src/day.20.ts
@@ -1,4 +1,5 @@
 import fs from "fs";
+import { clone } from "./utils/common";
 
 function parseInput() {
   return fs
@@ -6,14 +7,37 @@ function parseInput() {
     .toString()
     .split("\n")
     .filter((x) => x)
-    .map((x) => x);
+    .map((x, i) => ({ x: Number(x) * 811589153, i }));
 }
 
 export function part1() {
-  const input = parseInput();
-  console.log(input);
+  let input = parseInput();
 
-  return 0;
+  const original = clone(input);
+
+  for (let i = 0; i < 10; i++) {
+    for (const val of original) {
+      const idx = input.findIndex((a) => a.i === val.i && a.x === val.x);
+      input.splice(idx, 1);
+
+      const newIdx = (idx + val.x + input.length) % input.length;
+      input.splice(newIdx, 0, val);
+
+      if (val.x && newIdx === 0) {
+        input.push(input.shift()!);
+      }
+
+      //console.log(input);
+    }
+  }
+
+  const zero = input.findIndex((a) => a.x === 0)!;
+
+  return (
+    input[(zero + 1000) % input.length].x +
+    input[(zero + 2000) % input.length].x +
+    input[(zero + 3000) % input.length].x
+  );
 }
 
 export function part2() {
